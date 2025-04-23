c79692006e75c73e4ca01a4c57bdcd060eb5bd72
Hezi Lin <helin@bess-f2r3s14.clusters.42paris.fr>
2025-04-23 11:04:56 +0200

    googletest c code success

---
+++ math.c
@@ -0,0 +1,10 @@
+// math.c
+#include "math.h"
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
---
+++ math.h
@@ -0,0 +1,8 @@
+// math.h
+#ifndef MATH_H
+#define MATH_H
+
+int add(int a, int b);
+int subtract(int a, int b);
+
+#endif
